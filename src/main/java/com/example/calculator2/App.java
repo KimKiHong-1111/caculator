0a6eff7c4d3a0987f82e2670ccdfadb8c2a0b94b
Kihong_Kim <rlghd0157gmail.compwd>
2025-01-02 19:18:45 +0900

    lv2.계산기 90% 완성

--- src/main/java/com/example/calculator2/App.java
+++ src/main/java/com/example/calculator2/App.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("첫 번째 숫자를 입력하세요:");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요:");
-        int num2 = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요:");
-        char operator = sc.next().charAt(0);
-        Calculator calculator = new Calculator(sc.nextInt(),sc.nextInt());
-
-
-        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        String more= "";
+        System.out.print("계산할 숫자 2개와 사칙연산 기호를 입력하세요: ");
+        do {
+            Calculator calculator = new Calculator(sc.nextInt(),sc.nextInt(),sc.next());
+            calculator.operate(calculator.kiho);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            more = sc.next();
+        }while (!more.equals("exit"));
+        System.out.println("계산을 종료합니다.");
     }
 }
--- src/main/java/com/example/calculator2/Calculator.java
+++ src/main/java/com/example/calculator2/Calculator.java
@@ -1,17 +1,32 @@
 package com.example.calculator2;
 
 public class Calculator {
-   int num1;
-   int num2;
-   char operator;
+    int num1;
+    int num2;
+    String kiho;
+    int answer;
 
-    public Calculator(int num1, int num2) {
+
+    public Calculator(int num1, int num2, String kiho) {
         this.num1 = num1;
         this.num2 = num2;
+        this.kiho = kiho;
 
     }
 
-    public Calculator(char operator) {
-        this.operator = operator;
+
+    void operate(String kiho) {
+        if (kiho.equals("/") && num2 == 0) {
+            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+        } else {
+            switch (kiho) {
+                case "+" -> System.out.println("결과 : " + (answer = num1 + num2));
+                case "-" -> System.out.println("결과 : " + (answer = num1 - num2));
+                case "*" -> System.out.println("결과 : " + (answer = num1 * num2));
+                case "/" -> System.out.println("결과 : " + (answer = num1 / num2));
+                default -> System.out.println("사칙연산 기호를 확인해주세요");
+            }
+        }
     }
 }
+
