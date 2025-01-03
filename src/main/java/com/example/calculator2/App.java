200ecf40a7b53ca2d7d93833b4ce387b6f0df9e7
Kihong_Kim <rlghd0157gmail.compwd>
2025-01-03 15:14:56 +0900

    lv2 remove 구현

--- src/main/java/com/example/calculator2/App.java
+++ src/main/java/com/example/calculator2/App.java
@@ -2,22 +2,23 @@ package com.example.calculator2;
 
 import java.util.Scanner;
 
+
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("첫 번째 숫자를 입력하세요:");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요:");
-        int num2 = sc.nextInt();
-
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        String operator = sc.next();
-        String more= "";
+        String more = "";
         while (!more.equals("exit")) {
-            Calculator calculator = new Calculator(num1,num2,operator);
-            calculator.operate(calculator.operator);
+            System.out.print("첫 번째 숫자를 입력하세요:");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요:");
+            int num2 = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String operator = sc.next();
+
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            Calculator calculator = new Calculator(num1, num2, operator);
             more = sc.next();
+            if (more.equals("remove")) calculator.removeResult();
         }
         System.out.println("계산을 종료합니다.");
     }
--- src/main/java/com/example/calculator2/Calculator.java
+++ src/main/java/com/example/calculator2/Calculator.java
@@ -1,32 +1,41 @@
 package com.example.calculator2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Calculator {
-    int num1;
-    int num2;
-    String operator;
-    int answer;
+    private Queue<Integer>answerque = new LinkedList<>();
 
 
     public Calculator(int num1, int num2, String operator) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.operator = operator;
+        int answer = 0;
 
+        switch (operator) {
+            case "+" -> System.out.println("결과 : " + (answer = num1 + num2));
+            case "-" -> System.out.println("결과 : " + (answer = num1 - num2));
+            case "*" -> System.out.println("결과 : " + (answer = num1 * num2));
+            case "/" -> {
+                if (num2== 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }else System.out.println("결과 : " + (answer = num1 / num2));
+                answerque.add(answer);
+            }
+            default -> System.out.println("사칙연산 기호를 확인해주세요");
+        }
+    }
+
+    public void removeResult() {
+        answerque.poll();
     }
 
 
-    void operate(String operator) {
-        if (operator.equals("/") && num2 == 0) {
-            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-        } else {
-            switch (operator) {
-                case "+" -> System.out.println("결과 : " + (answer = num1 + num2));
-                case "-" -> System.out.println("결과 : " + (answer = num1 - num2));
-                case "*" -> System.out.println("결과 : " + (answer = num1 * num2));
-                case "/" -> System.out.println("결과 : " + (answer = num1 / num2));
-                default -> System.out.println("사칙연산 기호를 확인해주세요");
-            }
-        }
+
+    public Queue<Integer> getAnswerque() {
+        return answerque;
+    }
+
+    public void setAnswerque(Queue<Integer> answerque) {
+        this.answerque = answerque;
     }
 }
 
