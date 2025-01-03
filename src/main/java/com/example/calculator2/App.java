164959c1e51701469e236ca4e1ad3ce0c2dac374
Kihong_Kim <rlghd0157gmail.compwd>
2025-01-03 14:04:56 +0900

    lv2 계산기 완료

--- src/main/java/com/example/calculator2/App.java
+++ src/main/java/com/example/calculator2/App.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 숫자를 입력하세요:");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요:");
+        int num2 = sc.nextInt();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        String operator = sc.next();
         String more= "";
-        System.out.print("계산할 숫자 2개와 사칙연산 기호를 입력하세요: ");
         do {
-            Calculator calculator = new Calculator(sc.nextInt(),sc.nextInt(),sc.next());
-            calculator.operate(calculator.kiho);
+            Calculator calculator = new Calculator(num1,num2,operator);
+            calculator.operate(calculator.operator);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             more = sc.next();
         }while (!more.equals("exit"));
--- src/main/java/com/example/calculator2/Calculator.java
+++ src/main/java/com/example/calculator2/Calculator.java
@@ -3,23 +3,23 @@ package com.example.calculator2;
 public class Calculator {
     int num1;
     int num2;
-    String kiho;
+    String operator;
     int answer;
 
 
-    public Calculator(int num1, int num2, String kiho) {
+    public Calculator(int num1, int num2, String operator) {
         this.num1 = num1;
         this.num2 = num2;
-        this.kiho = kiho;
+        this.operator = operator;
 
     }
 
 
-    void operate(String kiho) {
-        if (kiho.equals("/") && num2 == 0) {
+    void operate(String operator) {
+        if (operator.equals("/") && num2 == 0) {
             System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
         } else {
-            switch (kiho) {
+            switch (operator) {
                 case "+" -> System.out.println("결과 : " + (answer = num1 + num2));
                 case "-" -> System.out.println("결과 : " + (answer = num1 - num2));
                 case "*" -> System.out.println("결과 : " + (answer = num1 * num2));
